986c33eb81bfe5558e4f3639f7c4d5a57329b8ff
chenTao <chentao@xiniaoyun.com>
2020-08-18 18:34:13 +0800

    Controller  Get装饰器

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, HttpCode, Header, Redirect, Param,HostParam } from '@nestjs/common';
 import { AppService } from './app.service';
+import {Request} from 'express';
 
-@Controller()
+@Controller('cats/all')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  // @Redirect('https://www.baidu.com', 302)
+  @Header('X-Powered-By', 'chentao')
+  getAllCats(@Param('id') params): string {
+    return this.appService.getHello() + `${params}`;
   }
 }
+
+@Controller({ host: ':account.example.com' })
+export class AccountController {
+  @Get()
+  getInfo(@HostParam('account') account: string) {
+    return account;
+  }
+}
+
